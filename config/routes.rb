f7db4462a8a36f8af8c91d0ad1f43063c0655d0a
jakenildo <jake.giangan@gmail.com>
2020-10-13 20:44:47 +0800

    Added Signup Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
+
   root 'static_pages#home'
   
   get 'static_pages/home'
   get 'static_pages/setting'
   
+  get '/home', to: 'static_pages#home'
+  get '/signup', to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
