2dbf0ffa98b700cf43901e0181e575bdfc1eb6d2
jakenildo <jake.giangan@gmail.com>
2020-10-13 23:59:24 +0800

    added userslist

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true,
-  length: { minimum: 5 }
+  length: { minimum: 3 }
 
   VALID_EMAIL_REGEX =
   /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create!(name: "Jake Nildo Giangan",
+            email: "jake@email.com",
+            password: "password",
+            password_confirmation: "password",
+            account_type: 1)
+
+50.times do |n|
+    name = Faker::Games::Pokemon.name
+    email = "username#{n+1}@email.com"
+    password = "password"
+    account_type = 0
+
+    User.create!(name: name,
+        email: email,
+        password: password,
+        password_confirmation: password,
+        account_type: 0)
+end
