293d908407659d6dc6eb03750b96bea50679afc2
jakenildo <72554032+jakenildo@users.noreply.github.com>
2020-10-14 19:28:53 +0800

    Added Login Module (#2)
    * Added Login Module
    
    * fixed indentions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create,:destroy]
 
   root 'static_pages#home'
   
@@ -8,6 +9,10 @@ Rails.application.routes.draw do
   
   get '/home', to: 'static_pages#home'
   get '/signup', to: 'users#new'
+  get '/profile', to: 'users#show'
+  
+  get '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
