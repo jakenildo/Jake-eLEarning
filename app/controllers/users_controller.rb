74613510fee2d03893e301f8fafca49b5b45d381
jakenildo <jake.giangan@gmail.com>
2020-10-14 22:01:08 +0800

    made settings page into update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :only_loggedin_users, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -6,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      redirect_to login_url
     else
       render 'new'
     end
@@ -28,9 +31,27 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email,:account_type, :password,
     :password_confirmation)
+  end
+
+  def only_loggedin_users
+    redirect_to login_url unless logged_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?  (@user)
   end
 end
