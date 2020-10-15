9cdf7db5e1428e3f1caf915e5ef712fa446f3e8e
jakenildo <72554032+jakenildo@users.noreply.github.com>
2020-10-15 19:47:48 +0800

    Update User module (#4)
    * partially worked on the settings
    
    * made settings page into update user
    
    * Added Flash Messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,17 @@ class SessionsController < ApplicationController
       if user &&
         user.authenticate(params[:session][:password])
         log_in user
+        flash[:success] = "Welcome back! (^_^) "
         redirect_to user
       else
+        flash[:danger] = "Login failed (X_X) "
         render 'new'
       end
   end
 
   def destroy
     log_out
+    flash[:success] = "Good bye! (^_^)/"
     redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :only_loggedin_users, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -6,8 +9,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      flash[:success] = "Sign-up Success!"
+      redirect_to login_url
     else
+      flash[:warning] = "Sign-up Failed"
       render 'new'
     end
   end
@@ -24,9 +29,33 @@ class UsersController < ApplicationController
       params[:page], per_page: 10 )
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Info Updated!"
+      redirect_to @user
+    else
+      flash[:warning] = "Update Failed!"
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email,:account_type, :password,
     :password_confirmation)
+  end
+
+  def only_loggedin_users
+    redirect_to login_url unless logged_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?  (@user)
   end
 end
