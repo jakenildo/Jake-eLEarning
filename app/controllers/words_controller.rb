730c7d7a203beda272ff0a17bd3247b457472324
jakenildo <jake.giangan@gmail.com>
2020-10-20 19:41:50 +0800

    Added Update & Delete Words & Choices function

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -14,29 +14,45 @@ class WordsController < ApplicationController
   def create
     @word = Word.new(word_params)
     if @word.save
-      flash[:success] = "Word & Choices added to Lesson"
+      flash[:success] = "Word & Choices added to Lesson! ^o^/"
       redirect_to categ_words_url(@word.category_id)
     else
-      flash[:warning] = "Failed to insert choice into database"
+      flash[:danger] = "Failed to insert choice into database :("
       redirect_to categ_words_url(@word.category_id)
     end
   end
 
-  def show
-  end
-
   def edit
+    @word = Word.find(params[:id])
+    @category_name = Category.find(@word.category_id)
   end
 
   def update
+    @word = Word.find(params[:id])
+    if @word.update_attributes(word_params)
+      flash[:success] = "Word & Choices Updated! ^o^/"
+      redirect_to categ_words_url(@word.category_id)
+    else
+      flash[:danger] = "Failed to update Word & Choice into database :("
+      redirect_to categ_words_url(@word.category_id)
+    end
   end
 
-  def delete
+
+  def destroy
+    @temp = Word.find(params[:id])
+    if Word.find(params[:id]).destroy
+    flash[:danger] = "#{@temp.words} & its Choices are deleted successfully! ^o^/ "
+      redirect_to categ_words_url(@temp.category_id)
+    else
+      flash[:warning] = "Deletion Failed! :("
+      redirect_to categ_words_url(@temp.category_id)
+    end
   end
 
   private
   def word_params
-    params.require(:word).permit(:category_id,:words, choices_attributes: [:choices, :correct_ans])
+    params.require(:word).permit(:category_id,:words, choices_attributes: [:id, :word_id, :choices, :correct_ans])
   end
   
 end
