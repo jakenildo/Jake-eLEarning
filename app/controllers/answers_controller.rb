cb7e91c8c6a6c09765a0b076ae71c4e0e7aaba55
jakenildo <jake.giangan@gmail.com>
2020-10-22 21:50:12 +0800

    implemented 1 item per page, but can't move to next page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,14 +2,16 @@ class AnswersController < ApplicationController
   def new
     @answers = Answer.new
     @categories = Category.find(params[:category_id])
-    @words = Word.where(category_id: params[:category_id])
+    @lesson_id = Lesson.last
+    @words = Word.where(category_id: params[:category_id]).paginate(page: params[:page], per_page: 1 )
   end
 
   def create
     @answers = Answer.new(ans_params)
     if @answers.save
       flash[:success] = "Answer Saved!"
-      redirect_to learn_lesson_url(@answers.lesson_id)
+      @categ_id = Lesson.find(@answers.lesson_id)
+      redirect_to learn_lesson_url(@categ_id.category_id)
     else
       flash[:warning] = "Failed to Save Answer!"
       redirect_to lessons_url
