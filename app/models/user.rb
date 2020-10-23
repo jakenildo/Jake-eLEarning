15daa399b956249ed5f9cf29a118d98fb0902bb0
jakenildo <72554032+jakenildo@users.noreply.github.com>
2020-10-23 20:47:22 +0800

    Lessons List (Road to Answering the Questionnaire) (#13)
    * Added Checkbox Checker
    
    * Changed the validations for the checkboxes[previous version is buggy]
    
    * initial commit
    
    * implemented lesosns list and temporary questionnaire page(will move it on the next branch
    
    * fixed issue when using db:seed
    
    * fixed the New Lesson button for admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true,
   length: { minimum: 3 }
-
   VALID_EMAIL_REGEX =
   /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -11,4 +10,5 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, length: {minimum: 6}
+
 end
